db0e96bf43d40337bec724e9173cfdecb61a66f8
Paul Revelo <Paul Revelo>
2016-04-15 16:43:44 +0800

    first commit

---
+++ app/modules/dashboard/dashboard.js
@@ -0,0 +1,13 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('dashboardController', dashboardController);
+
+	dashboardController.$inject = [];
+
+	function dashboardController() {
+		// body...
+	}
+})();
